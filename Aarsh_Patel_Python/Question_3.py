42a0c756f5347c24870ddba8d7e1e44169e5406e
Aarsh Patel <32023476+aarsh2000@users.noreply.github.com>
2018-08-29 19:08:04 -0400

    Update Question_3.py

--- Aarsh_Patel_Python/Question_3.py
+++ Aarsh_Patel_Python/Question_3.py
@@ -1,41 +1,42 @@
+'''
+3.	Write a program that takes a number (1-100) and determines whether this is a product of two Prime numbers.
+If yes, print the two Prime numbers, otherwise print a “try again” message. Allow the user 3 tries.
+'''
 primenum = []
-product = []
-for i in range(10):
-    product.append(0)
-    
-def prime(num):
-    for i in range(num):
-        if i!= 0 and i!= num and i!= 1:
-            if  num%i==0:
-                return True
-            else:
-                return False
-    
 
-for i in range(100):
-    if prime(i) == False:
-         primenum.append(i)
-    
-for i in range(len(primenum)):
-    for j in range(len(primenum)):
-        product.append(primenum[i]*primenum[j])
-        
+def findPrime(until):
+  for num in range(until+1):
+    isPrime = True
+    for i in range(num):
+        if i != 0 and i != 1 and num % i == 0:
+            isPrime = False
+        if isPrime and i == num-1:
+            primenum.append(num)
+
+findPrime(100)
+
+def checkProduct(product):
+    product = int(product)
+    for num in range(len(primenum)):
+            value = int(product) / int(primenum[num])
+            for numb in primenum:
+                if value == numb:
+                    print(str(primenum[num])+" "+str(numb))
+                    return True
+
+
+    return False
+
 
 
 attempts = 3
 
-answer = False
-while attempts>0:
-    number = input(str(attempts) + " Enter a value: ")
-    for i in range(len(product)):
-        if number == product[i]:
-            answer = True
-        else:
-            answer = False
 
-    if answer:
-        print("yes")
-    else:
+while attempts>0:
+    number = input(str(attempts) + " Enter a product: ")
+    if checkProduct(number) == False:
         print("try again")
-         
+
+
+
     attempts -=1
