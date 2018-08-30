9734e5e4c32fb380a7fa6d2e1b778ce705b8f785
Aarsh Patel <32023476+aarsh2000@users.noreply.github.com>
2018-08-30 01:57:53 -0400

    Add files via upload

---
+++ Aarsh_Patel_Python/Question_5.py
@@ -0,0 +1,21 @@
+
+word = input("Enter word: ")
+backwords = ""
+i = len(word)-1
+
+while i >= 0:
+    backwords = backwords + word[i]
+    i -= 1
+
+answer = True
+
+for i in range(len(word)):
+    if word[i] == backwords[i]:
+        answer = True
+    else:
+        answer = False
+
+if not(answer):
+    print("Not palindrome")
+else:
+    print("Is palindrome")
